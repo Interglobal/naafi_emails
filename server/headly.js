ecf828e9a63caea7a6e2c62c4ae96ce65defb6bd
Patrick Best <patrickbest@patrickbest.com>
2014-06-18 18:54:37 -0500

    fb og headly crap

--- server/headly.js
+++ server/headly.js
@@ -1,9 +1,20 @@
 Meteor.headly.config({
 	data: function(req) {
-		console.log(req);
-		return {
-			title: 'this'
-		};
+		console.log(req.url);
+		console.log(req._parsedUrl);
+
+		if (req._parsedUrl.path === '/') {
+
+			var currentRelease = Releases.findOne({},{timestamp: -1});
+
+			return {
+				title: 'N.A.A.F.I Downloads - ' + currentRelease.title
+			}
+		} else {
+			return {
+				title: 'N.A.A.F.I Downloads'
+			}
+		}
 	},
 	facebook: function(data) {
 		return '<meta property="og:title" content="' + data.title + '" />';
